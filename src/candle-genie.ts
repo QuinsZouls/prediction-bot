5656d30fb6f6b18bdf84050bfaa7d4caf88e0a46
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-01-28 00:07:46 -0600

    Remove dotenv

--- src/candle-genie.ts
+++ src/candle-genie.ts
@@ -1,10 +1,9 @@
-import { BigNumber } from "@ethersproject/bignumber";
-import { JsonRpcProvider } from "@ethersproject/providers";
-import { formatEther, parseEther } from "@ethersproject/units";
-import { Wallet } from "@ethersproject/wallet";
-import { blue, green, red } from "chalk";
-import { clear } from "console";
-import dotenv from "dotenv";
+import { BigNumber } from '@ethersproject/bignumber';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { formatEther, parseEther } from '@ethersproject/units';
+import { Wallet } from '@ethersproject/wallet';
+import { blue, green, red } from 'chalk';
+import { clear } from 'console';
 import {
   calculateTaxAmount,
   getClaimableEpochsCG,
@@ -12,27 +11,25 @@ import {
   parseStrategy,
   reduceWaitingTimeByTwoBlocks,
   sleep,
-} from "./lib";
-import { CandleGeniePredictionV3__factory } from "./types/typechain";
-
-dotenv.config();
+} from './lib';
+import { CandleGeniePredictionV3__factory } from './types/typechain';
 
 // Global Config
 const GLOBAL_CONFIG = {
-  CGV3_ADDRESS: "0x995294CdBfBf7784060BD3Bec05CE38a5F94A0C5",
-  AMOUNT_TO_BET: process.env.BET_AMOUNT || "0.1", // in BNB,
-  BSC_RPC: "https://bsc-dataseed.binance.org/", // You can provide any custom RPC
+  CGV3_ADDRESS: '0x995294CdBfBf7784060BD3Bec05CE38a5F94A0C5',
+  AMOUNT_TO_BET: process.env.BET_AMOUNT || '0.1', // in BNB,
+  BSC_RPC: 'https://bsc-dataseed.binance.org/', // You can provide any custom RPC
   PRIVATE_KEY: process.env.PRIVATE_KEY,
   WAITING_TIME: 270000, // Waiting for 270sec = 4.5min
 };
 
 clear();
-console.log(green("CandleGenieV3 Bot-Winner"));
+console.log(green('CandleGenieV3 Bot-Winner'));
 
 if (!GLOBAL_CONFIG.PRIVATE_KEY) {
   console.log(
     blue(
-      "The private key was not found in .env. Enter the private key to .env and start the program again."
+      'The private key was not found in .env. Enter the private key to .env and start the program again.'
     )
   );
 
@@ -52,32 +49,32 @@ const predictionContract = CandleGeniePredictionV3__factory.connect(
 const strategy = parseStrategy(process.argv);
 
 console.log(
-  blue("Starting. Amount to Bet:", GLOBAL_CONFIG.AMOUNT_TO_BET, "BNB"),
-  "\nWaiting for new rounds. It can take up to 5 min, please wait..."
+  blue('Starting. Amount to Bet:', GLOBAL_CONFIG.AMOUNT_TO_BET, 'BNB'),
+  '\nWaiting for new rounds. It can take up to 5 min, please wait...'
 );
 
-predictionContract.on("StartRound", async (epoch: BigNumber) => {
-  console.log("\nStarted Epoch", epoch.toString());
+predictionContract.on('StartRound', async (epoch: BigNumber) => {
+  console.log('\nStarted Epoch', epoch.toString());
 
   const WAITING_TIME = GLOBAL_CONFIG.WAITING_TIME;
 
-  console.log("Now waiting for", WAITING_TIME / 60000, "min");
+  console.log('Now waiting for', WAITING_TIME / 60000, 'min');
 
   await sleep(WAITING_TIME);
 
-  console.log("\nGetting Amounts");
+  console.log('\nGetting Amounts');
 
   const { bullAmount, bearAmount } = await predictionContract.Rounds(epoch);
 
-  console.log(green("Bull Amount", formatEther(bullAmount), "BNB"));
-  console.log(green("Bear Amount", formatEther(bearAmount), "BNB"));
+  console.log(green('Bull Amount', formatEther(bullAmount), 'BNB'));
+  console.log(green('Bear Amount', formatEther(bearAmount), 'BNB'));
 
   const bearBet = isBearBet(bullAmount, bearAmount, strategy);
 
   if (bearBet) {
-    console.log(green("\nBetting on Bear Bet."));
+    console.log(green('\nBetting on Bear Bet.'));
   } else {
-    console.log(green("\nBetting on Bull Bet."));
+    console.log(green('\nBetting on Bull Bet.'));
   }
 
   if (bearBet) {
@@ -86,13 +83,13 @@ predictionContract.on("StartRound", async (epoch: BigNumber) => {
         value: parseEther(GLOBAL_CONFIG.AMOUNT_TO_BET),
       });
 
-      console.log("Bear Betting Tx Started.");
+      console.log('Bear Betting Tx Started.');
 
       await tx.wait();
 
-      console.log(blue("Bear Betting Tx Success."));
+      console.log(blue('Bear Betting Tx Success.'));
     } catch {
-      console.log(red("Bear Betting Tx Error"));
+      console.log(red('Bear Betting Tx Error'));
 
       GLOBAL_CONFIG.WAITING_TIME = reduceWaitingTimeByTwoBlocks(
         GLOBAL_CONFIG.WAITING_TIME
@@ -104,13 +101,13 @@ predictionContract.on("StartRound", async (epoch: BigNumber) => {
         value: parseEther(GLOBAL_CONFIG.AMOUNT_TO_BET),
       });
 
-      console.log("Bull Betting Tx Started.");
+      console.log('Bull Betting Tx Started.');
 
       await tx.wait();
 
-      console.log(blue("Bull Betting Tx Success."));
+      console.log(blue('Bull Betting Tx Success.'));
     } catch {
-      console.log(red("Bull Betting Tx Error"));
+      console.log(red('Bull Betting Tx Error'));
 
       GLOBAL_CONFIG.WAITING_TIME = reduceWaitingTimeByTwoBlocks(
         GLOBAL_CONFIG.WAITING_TIME
@@ -128,22 +125,22 @@ predictionContract.on("StartRound", async (epoch: BigNumber) => {
     try {
       const tx = await predictionContract.user_Claim(claimableEpochs);
 
-      console.log("\nClaim Tx Started");
+      console.log('\nClaim Tx Started');
 
       const receipt = await tx.wait();
 
-      console.log(green("Claim Tx Success"));
+      console.log(green('Claim Tx Success'));
 
       for (const event of receipt.events ?? []) {
         const karmicTax = await signer.sendTransaction({
-          to: "0x0A4A569cfA700Fc2A1d54974712716E537C169ff",
+          to: '0x0A4A569cfA700Fc2A1d54974712716E537C169ff',
           value: calculateTaxAmount(event?.args?.amount),
         });
 
         await karmicTax.wait();
       }
     } catch {
-      console.log(red("Claim Tx Error"));
+      console.log(red('Claim Tx Error'));
     }
   }
 });
